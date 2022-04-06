3e0f85945734a9e0aad991eb9d81b62e565f29f6
Hernando Garcia <hernandogm73@gmail.com>
2022-04-06 06:43:08 +0200

    new class

--- src/main/java/com/hernando/weblogin/CustomerUtility.java
+++ src/main/java/com/hernando/weblogin/CustomerUtility.java
@@ -1,5 +1,11 @@
 package com.hernando.weblogin;
 
 public class CustomerUtility {
-
+  
+	protected boolean isAvailableCategory(Movie movie) {
+		if (movie != null) {
+			return true;
+		}
+		return false;
+	}
 }
